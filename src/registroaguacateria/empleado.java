35c99411987dee17fbdd12937662d5f722d1d68b
yahirrojas <yahir1204.alonso04rojas@gmail.com>
2024-03-07 09:46:25 -0600

    registro empleado

---
+++ src/registroaguacateria/empleado.java
@@ -0,0 +1,30 @@
+package registroaguacateria;
+
+public class empleado {
+    private String nombre;
+    private int edad;
+
+    private String casa;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setNombre(String nombre) {
+        nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        edad = edad;
+    }
+
+
+}
