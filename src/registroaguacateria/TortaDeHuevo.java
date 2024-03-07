725adb67fbf3cdb675ad070b7a9793192f5d141f
Yote242 <jebc201321@gmail.com>
2024-03-07 14:59:55 -0600

    Clase torta de huevo

---
+++ src/registroaguacateria/TortaDeHuevo.java
@@ -0,0 +1,31 @@
+package registroaguacateria;
+
+public class TortaDeHuevo {
+    private String huevo;
+    private String pan;
+    private String salsa;
+
+    public String getHuevo() {
+        return huevo;
+    }
+
+    public void setHuevo(String huevo) {
+        this.huevo = huevo;
+    }
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+
+    public String getSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(String salsa) {
+        this.salsa = salsa;
+    }
+}
